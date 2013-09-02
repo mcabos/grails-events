f5d0980a809bde24d6796fca0001f44a0ca4ce98
Stephane Maldini <smaldini@gopivotal.com>
2013-09-03 00:47:33 +0100

    open ConsumerFilter for reuse

--- src/groovy/org/grails/plugins/events/reactor/configuration/ConsumerBeanPostProcessor.groovy
+++ src/groovy/org/grails/plugins/events/reactor/configuration/ConsumerBeanPostProcessor.groovy
@@ -31,67 +31,27 @@ import static reactor.event.selector.Selectors.*
  * @author Stephane Maldini
  */
 @CompileStatic
-class ConsumerBeanPostProcessor implements BeanPostProcessor, Ordered {
-	private static final Logger LOG = LoggerFactory.getLogger(ConsumerBeanPostProcessor)
-	public static final ReflectionUtils.MethodFilter CONSUMER_METHOD_FILTER = new ReflectionUtils.MethodFilter() {
-		@Override
-		boolean matches(Method method) {
-			AnnotationUtils.findAnnotation method, Selector
-		}
-	}
-
-	private final ConversionService conversionService
-
+class ConsumerBeanPostProcessor extends reactor.spring.beans.factory.config.ConsumerBeanPostProcessor {
 
 	EventsApi eventsApi
 
 	ConsumerBeanPostProcessor() {
-		this.conversionService = new DefaultFormattingConversionService()
-	}
-
-	@Autowired(required=false)
-	ConsumerBeanPostProcessor(ConversionService conversionService) {
-		this.conversionService = conversionService
+		super()
 	}
 
 	@Override
-	int getOrder() {
-		LOWEST_PRECEDENCE
-	}
-
-	@Override
-	def postProcessBeforeInitialization(bean,
-	                                    String beanName) throws BeansException {
-		bean
-	}
-
-	@Override
-	def postProcessAfterInitialization(final bean,
-	                                   String beanName) throws BeansException {
-
-		initBean bean, C.findHandlerMethods(bean.getClass(), CONSUMER_METHOD_FILTER)
-	}
-
-	def initBean(final bean, Set<Method> methods) {
-		if(!methods)
-			return bean
-
+	Object initBean(Object bean, Set<Method> methods) {
+		//clean existing consumers for this service
 		Consumer consumer
 		Reactor reactor
 		Selector selectorAnno
-		ReplyTo replyToAnno
-		S selector
+		reactor.event.selector.Selector selector
 
-		def scanAnnotation = { Method method ->
-			selectorAnno = AnnotationUtils.findAnnotation(method, Selector)
-			replyToAnno = AnnotationUtils.findAnnotation(method, ReplyTo)
-			reactor = fetchReactor(selectorAnno)
-			selector = fetchSelector(selectorAnno, method)
-		}
-
-		//clean existing consumers for this service
 		for (final Method method : methods) {
-			scanAnnotation method
+			//scanAnnotation method
+			selectorAnno = AnnotationUtils.findAnnotation(method, Selector.class);
+			reactor = fetchObservable(selectorAnno, bean)
+			selector = fetchSelector(selectorAnno, bean, method);
 
 			for (registration in reactor.consumerRegistry.select(selector.object)) {
 				consumer = registration.object
@@ -103,140 +63,29 @@ class ConsumerBeanPostProcessor implements BeanPostProcessor, Ordered {
 			}
 		}
 
-		for (final Method method : methods) {
-			scanAnnotation method
-
-			//register [replyTo]consumer
-			String replyTo = replyToAnno?.value()
-			Invoker handler = new Invoker(method, bean)
-			consumer = replyToAnno ?
-					new ReplyToServiceConsumer(reactor, replyTo, handler) :
-					new ServiceConsumer(handler)
-
-			if (LOG.debugEnabled) {
-				LOG.debug("Attaching Consumer to Reactor[" + reactor + "] using Selector[" + selector + "]")
-			}
-
-			if (!selector) {
-				replyToAnno ? reactor.on(consumer) :
-			} else {
-				reactor.on(selector, consumer)
-			}
-		}
-		bean
+		super.initBean(bean, methods)
 	}
 
-	private Reactor fetchReactor(Selector selectorAnno) {
+	@Autowired(required = false)
+	ConsumerBeanPostProcessor(ConversionService conversionService) {
+		super(conversionService)
+	}
+
+	@Override
+	protected Reactor fetchObservable(Selector selectorAnno, Object bean) {
 		selectorAnno.reactor() == 'reactor' ?
 				eventsApi.appReactor :
 				eventsApi.groovyEnvironment[selectorAnno.reactor()]
 	}
 
-	private S fetchSelector(Selector selectorAnno, Method method) {
-		String sel = selectorAnno.value() ?: method.name
-		try {
-			switch (selectorAnno.type()) {
-				case SelectorType.OBJECT:
-					return object(sel)
-				case SelectorType.REGEX:
-					return regex(sel)
-				case SelectorType.URI:
-					return uri(sel)
-				case SelectorType.TYPE:
-					try {
-						return type(Class.forName(sel))
-					} catch (ClassNotFoundException e) {
-						throw new IllegalArgumentException(e.message, e)
-					}
-			}
-		} catch (EvaluationException e) {
-			if (LOG.isTraceEnabled()) {
-				LOG.trace("Creating ObjectSelector for '" + sel + "' due to " + e.message, e)
-			}
-			return object(sel)
-		}
+	@Override
+	protected Object parseSelector(Selector selector, Object bean, Method method) {
+		selector.value() ?: method.name
 	}
 
-	private final static class ReplyToServiceConsumer implements Consumer<Event> {
-
-		final Reactor reactor
-		final Object replyToKey
-		final Invoker handler
-
-		ReplyToServiceConsumer(Reactor reactor, Object replyToKey, Invoker handler) {
-			this.reactor = reactor
-			this.replyToKey = replyToKey
-			this.handler = handler
-		}
-
-		@Override
-		void accept(Event ev) {
-			def result = handler.apply(ev)
-			def _replyToKey = replyToKey ?: ev.replyTo
-			if(_replyToKey)
-				reactor.notify _replyToKey, Event.wrap(result)
-		}
+	@Override
+	protected Object parseReplyTo(ReplyTo selector, Object bean) {
+		selector.value()
 	}
 
-	private final static class ServiceConsumer implements Consumer<Event> {
-		final Invoker handler
-
-		ServiceConsumer(Invoker handler) {
-			this.handler = handler
-		}
-
-		@Override
-		void accept(Event ev) {
-			handler.apply(ev)
-		}
-	}
-
-	private final class Invoker implements Function<Event, Object> {
-
-		final Method method
-
-		final Object bean
-
-		Invoker(Method method, Object bean) {
-			this.method = method
-			this.bean = bean
-		}
-		Class<?>[] argTypes = method.parameterTypes
-
-		@Override
-		def apply(Event ev) {
-			if (argTypes.length == 0) {
-				if (LOG.debugEnabled) {
-					LOG.debug("Invoking method[" + method + "] on " + bean + " using " + ev)
-				}
-				return ReflectionUtils.invokeMethod(method, bean)
-			}
-
-			if (argTypes.length > 1) {
-				throw new IllegalStateException("Multiple parameters not yet supported.")
-			}
-
-			if (null == ev.data || argTypes[0].isAssignableFrom(ev.data.getClass())) {
-				if (LOG.debugEnabled) {
-					LOG.debug("Invoking method[" + method + "] on " + bean + " using " + ev.data)
-				}
-				return ReflectionUtils.invokeMethod(method, bean, ev.data)
-			}
-
-			if (!argTypes[0].isAssignableFrom(ev.getClass())
-					&& conversionService.canConvert(ev.getClass(), argTypes[0])) {
-				ReflectionUtils.invokeMethod(method, bean, conversionService.convert(ev, argTypes[0]))
-			}
-
-			if (conversionService.canConvert(ev.data.getClass(), argTypes[0])) {
-				Object convertedObj = conversionService.convert(ev.data, argTypes[0])
-				if (LOG.debugEnabled) {
-					LOG.debug("Invoking method[" + method + "] on " + bean + " using " + convertedObj)
-				}
-				return ReflectionUtils.invokeMethod(method, bean, convertedObj)
-			}
-
-			throw new IllegalArgumentException("Cannot invoke method " + method + " passing parameter " + ev.data)
-		}
-	}
 }
